bb7c40f8ad6908d6b837d470e1a9d6803629ef74
laduiw <wzy.laduiw@gmail.com>
2023-11-28 12:46:11 +0800

    lazy allocation finished

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,18 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  //if(growproc(n) < 0)
+ //   return -1;
+  uint sz = addr;
+  if (n>0)
+  {
+ 	 myproc()->sz += n;
+  }
+  else {
+	if (sz+n>0) { sz = uvmdealloc(myproc()->pagetable, sz, sz + n); myproc()->sz = sz;}
+		else return -1;
+  }
+
   return addr;
 }
 
